14541b99da279fe578bbc1fe6a908d31f969f399
Lauren Golden <llgolden@gmail.com>
2023-04-05 09:13:51 -0600

    first tests

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -6,27 +6,20 @@ require './lib/passenger'
 RSpec.describe Passenger do
   it "exists" do
     charlie = Passenger.new({name: "Charlie", age: 18})
+    taylor = Passenger.new({name: "Taylor", age: 12})
 
     expect(charlie).to be_a Passenger
+    expect(charlie.name).to eq("Charlie")
+    expect(charlie.age).to eq(18)
+
   end
 
 
 
 end
 
-# @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
 
-# pry(main)> charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
-# # => #<Passenger:0x00007fc1ad88b3c0...>
 
-# pry(main)> taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
-# # => #<Passenger:0x00007fe0da2cf1b0...>
-
-# pry(main)> charlie.name
-# # => "Charlie"
-
-# pry(main)> charlie.age
-# # => 18
 
 # pry(main)> charlie.adult?
 # # => true
