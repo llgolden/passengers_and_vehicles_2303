c92d0dccee187c1a187e51ea97442c0ce7ee8256
Lauren Golden <llgolden@gmail.com>
2023-04-05 10:08:54 -0600

    Add the num_adults method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -21,4 +21,13 @@ class Vehicle
     @passengers << person_name
   end
 
+  def num_adults
+    total_adults = 0
+    passengers.each do |passenger|
+      if passenger.passenger_details[:age] >= 18 
+        total_adults += 1
+      end
+    end
+    total_adults
+  end 
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -29,13 +29,18 @@ RSpec.describe Vehicle do
     vehicle.add_passenger(jude)    
     vehicle.add_passenger(taylor)   
     expect(vehicle.passengers).to eq([charlie, jude, taylor])
+    require 'pry'; binding.pry
   end
 
-
+  it "can count the number of adults" do
+    vehicle = Vehicle.new("2001", "Honda", "Civic") 
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+    jude = Passenger.new({"name" => "Jude", "age" => 20})    
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})   
+    vehicle.add_passenger(charlie)    
+    vehicle.add_passenger(jude)    
+    vehicle.add_passenger(taylor)  
+  
+    expect(vehicle.num_adults).to eq(2)
+  end
 end
-
-# expect().to eq()
-
-
-# pry(main)> vehicle.num_adults
-# # => 2
